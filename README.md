5a9cfd263d1579cbde2850e78ba283471dc2d5be
Malindi T <hp.asd6@gmail.com>
2024-03-05 18:07:16 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ResumeCrafts
+360ResumeCraft is a free and quick site that makes the hassle of creating a Resume easy and quick, you can quickly create a resume in under 15 minutes. It provides a simple gui and does it's operations locally.
+After crafting the Resume you can simply print out the document as pdf, and choose to save or share it.
+
+No user information is collect or processed outside the local space at all, as soon as the site has fully loaded everything happens locally.
+
+Visit the site : 360resume.neocities.org
---
+++ src/app.js
@@ -0,0 +1,35 @@
+
+const htmlContent = {
+    education: `<p class="school-name"></br>School name</p>
+                <p><span class="date">School date</span> | 
+                <span class="qualification">Qualification</span> -
+                <span class="study">Field of study</span></p>`,
+    experience: `<p></br><span class="title">Job title</span> | 
+                <span class="date">Work date</span></p>
+                <p><span class="company">Company name</span> - 
+                <span class="address">Company address</span></p>
+                <p class="duties">Work duties - click anywhere on this text to change or remove it.</p>`,
+    skills: `<li>Click to edit or remove skill</li>`,
+    reference: `<p></br><span class="position">Job title</span> |
+                <span class="fullname">Employer/employee name</span></p>
+                <p class="company">Company name</p>
+                <p class="contacts">Phone</p>`
+}
+//button events
+document.querySelectorAll('.add-btn').forEach((el) => {
+    el.addEventListener('click', () => {
+        targetId = el.getAttribute('id')
+        const targetEl = document.querySelector('.'+targetId)
+        targetEl.lastElementChild.insertAdjacentHTML('beforeend', htmlContent[targetId])
+    })
+})
+//pop-up message
+function displayPopUpMsg() {
+    const popUpMsg = document.createElement('p');
+        popUpMsg.setAttribute('id', 'pop-up-msg');
+        popUpMsg.innerHTML = 'Hi, please note! </br>Every text is editable, click on any text to remove or make changes.';
+        document.querySelector('main').append(popUpMsg);
+    setTimeout(() => popUpMsg.remove(), 5000);
+};
+
+document.addEventListener('DOMContentLoaded', displayPopUpMsg)
